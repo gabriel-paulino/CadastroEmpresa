85b84ef753a08e07ffa6c951172eb4bb69979878
gabrielpaulino <gabriel.paulino104@gmail.com>
2020-02-15 17:20:58 -0300

    Projeto versao desktop

---
+++ CadastroEmpresaDesktop/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CadastroEmpresaDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFantasia = new System.Windows.Forms.TextBox();
+            this.txtDataAbertura = new System.Windows.Forms.TextBox();
+            this.txtAtividadePrincipal = new System.Windows.Forms.TextBox();
+            this.txtCapitalSocial = new System.Windows.Forms.TextBox();
+            this.txtSituacao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CNPJ:";
+            this.label1.UseMnemonic = false;
+            // 
+            // txtCnpj
+            // 
+            this.txtCnpj.Location = new System.Drawing.Point(132, 17);
+            this.txtCnpj.Mask = "00,000,000/0000-00";
+            this.txtCnpj.Name = "txtCnpj";
+            this.txtCnpj.Size = new System.Drawing.Size(112, 20);
+            this.txtCnpj.TabIndex = 2;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnConsultar.Image = global::CadastroEmpresaDesktop.Properties.Resources.pesq;
+            this.btnConsultar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConsultar.Location = new System.Drawing.Point(275, 12);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(179, 28);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar Receita Federal";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_ClickAsync);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnBuscar.Image = global::CadastroEmpresaDesktop.Properties.Resources.pesq;
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(22, 272);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(92, 31);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnExcluir.Image = global::CadastroEmpresaDesktop.Properties.Resources.Eraser;
+            this.btnExcluir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExcluir.Location = new System.Drawing.Point(259, 272);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(92, 31);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAdicionar.Image = global::CadastroEmpresaDesktop.Properties.Resources.add;
+            this.btnAdicionar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdicionar.Location = new System.Drawing.Point(139, 272);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(92, 31);
+            this.btnAdicionar.TabIndex = 6;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSair.Image = global::CadastroEmpresaDesktop.Properties.Resources.exit;
+            this.btnSair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSair.Location = new System.Drawing.Point(372, 272);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(92, 31);
+            this.btnSair.TabIndex = 7;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nome Fantasia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Atividade Principal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Data Abertura";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(221, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Situação";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(26, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Capital Social";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(132, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(301, 20);
+            this.txtNome.TabIndex = 14;
+            // 
+            // txtFantasia
+            // 
+            this.txtFantasia.Location = new System.Drawing.Point(132, 100);
+            this.txtFantasia.Name = "txtFantasia";
+            this.txtFantasia.Size = new System.Drawing.Size(301, 20);
+            this.txtFantasia.TabIndex = 15;
+            // 
+            // txtDataAbertura
+            // 
+            this.txtDataAbertura.Location = new System.Drawing.Point(132, 181);
+            this.txtDataAbertura.Name = "txtDataAbertura";
+            this.txtDataAbertura.Size = new System.Drawing.Size(75, 20);
+            this.txtDataAbertura.TabIndex = 17;
+            // 
+            // txtAtividadePrincipal
+            // 
+            this.txtAtividadePrincipal.Location = new System.Drawing.Point(132, 141);
+            this.txtAtividadePrincipal.Name = "txtAtividadePrincipal";
+            this.txtAtividadePrincipal.Size = new System.Drawing.Size(301, 20);
+            this.txtAtividadePrincipal.TabIndex = 16;
+            // 
+            // txtCapitalSocial
+            // 
+            this.txtCapitalSocial.Location = new System.Drawing.Point(132, 220);
+            this.txtCapitalSocial.Name = "txtCapitalSocial";
+            this.txtCapitalSocial.Size = new System.Drawing.Size(182, 20);
+            this.txtCapitalSocial.TabIndex = 18;
+            // 
+            // txtSituacao
+            // 
+            this.txtSituacao.Location = new System.Drawing.Point(290, 181);
+            this.txtSituacao.Name = "txtSituacao";
+            this.txtSituacao.Size = new System.Drawing.Size(77, 20);
+            this.txtSituacao.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(489, 322);
+            this.Controls.Add(this.txtSituacao);
+            this.Controls.Add(this.txtCapitalSocial);
+            this.Controls.Add(this.txtDataAbertura);
+            this.Controls.Add(this.txtAtividadePrincipal);
+            this.Controls.Add(this.txtFantasia);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtCnpj);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro Empresa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txtCnpj;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFantasia;
+        private System.Windows.Forms.TextBox txtDataAbertura;
+        private System.Windows.Forms.TextBox txtAtividadePrincipal;
+        private System.Windows.Forms.TextBox txtCapitalSocial;
+        private System.Windows.Forms.TextBox txtSituacao;
+    }
+}
+
---
+++ CadastroEmpresaDesktop/Form1.cs
@@ -0,0 +1,61 @@
+﻿using CadastroEmpresa;
+using Refit;
+using System;
+using System.Windows.Forms;
+
+namespace CadastroEmpresaDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configurando Componentes ao executar app
+            btnAdicionar.Enabled = false;
+            btnBuscar.Enabled = true;
+            btnConsultar.Enabled = true;
+            btnExcluir.Enabled = false;
+            txtCnpj.Enabled = true;
+
+            txtNome.Enabled = false;
+            txtFantasia.Enabled = false;
+            txtAtividadePrincipal.Enabled = false;
+            txtDataAbertura.Enabled = false;
+            txtSituacao.Enabled = false;
+            txtCapitalSocial.Enabled = false;
+        }
+
+        private async void btnConsultar_ClickAsync(object sender, EventArgs e)
+        {
+            string cnpjInformado = txtCnpj.Text;
+
+            //Removendo os caracteres do cnpj: ','; '/' e '-' para busca na web api
+            cnpjInformado = cnpjInformado.Replace(".", "").Replace("/", "").Replace("-", "");
+
+            // Criando rota para acesso à api e salvar a resposta na variavel empresa
+            var cnpjEmpresa = RestService.For<EmpresaApiService>("https://www.receitaws.com.br");
+            var empresa = await cnpjEmpresa.GetEmpresaAsync(cnpjInformado);
+
+            //Mostrando alguns dados obtidos na web api
+
+            txtNome.Text = empresa.Nome;
+            txtFantasia.Text = empresa.Fantasia;
+            txtAtividadePrincipal.Text = empresa.AtividadePrincipal[0].Text;
+            txtDataAbertura.Text = empresa.Abertura;
+            txtSituacao.Text = empresa.Situacao;
+
+            double capitalSocial = Convert.ToDouble(empresa.CapitalSocial);
+            capitalSocial /= 100; 
+            txtCapitalSocial.Text = String.Format("{0:c}", capitalSocial);
+
+
+            btnBuscar.Enabled = false;
+            btnAdicionar.Enabled = true;
+
+        }
+    }
+}
